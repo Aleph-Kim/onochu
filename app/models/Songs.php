2065314b160737c3342da52ab01bac80cb23ad2b
김채민 <dktjdej25@gmail.com>
2025-03-23 20:52:59 +0900

    수정 - songs 테이블 가사, 작곡가, 작사가, 편곡가 컬럼 추가

--- app/models/Songs.php
+++ app/models/Songs.php
@@ -7,14 +7,18 @@ class Songs extends Model
     public function insert($song)
     {
         try {
-            $sql = "INSERT INTO songs (album_id, artist_id, title, genre, flo_id) 
-                    VALUES (:album_id, :artist_id, :title, :genre, :flo_id)";
+            $sql = "INSERT INTO songs (album_id, artist_id, title, genre, lyrics, composer, lyricist, arranger, flo_id) 
+                    VALUES (:album_id, :artist_id, :title, :genre, :lyrics, :composer, :lyricist, :arranger, :flo_id)";
             $stmt = $this->db->prepare($sql);
             $stmt->execute([
                 ':album_id' => $song['album_id'],
                 ':artist_id' => $song['artist_id'],
                 ':title' => $song['title'],
                 ':genre' => $song['genre'],
+                ':lyrics' => $song['lyrics'],
+                ':composer' => $song['composer'],
+                ':lyricist' => $song['lyricist'],
+                ':arranger' => $song['arranger'],
                 ':flo_id' => $song['flo_id']
             ]);
             return $this->db->lastInsertId();
