7880c159567d9383ad2b8f0918beee208c20610d
김채민 <dktjdej25@gmail.com>
2025-03-23 15:42:10 +0900

    오류 수정 - Model 파일의 전역 변수 접근 문제로 발생한 DB 연결 오류 수정

--- core/Model.php
+++ core/Model.php
@@ -1,18 +1,16 @@
 <?
 
-$dbHost = getenv("DB_HOST"); // 호스트 주소
-$dbName = getenv("DB_NAME"); // 데이타 베이스(DataBase) 이름
-$dbUser = getenv("DB_USER"); // DB 아이디
-$dbPass = getenv("DB_PASS"); // DB 패스워드
-$dbChar = getenv("DB_CHAR"); // 문자 인코딩
-
 class Model
 {
     protected $db;
 
     public function __construct()
     {
-        global $dbHost, $dbName, $dbUser, $dbPass, $dbChar;
+        $dbHost = getenv("DB_HOST"); // 호스트 주소
+        $dbName = getenv("DB_NAME"); // 데이타 베이스(DataBase) 이름
+        $dbUser = getenv("DB_USER"); // DB 아이디
+        $dbPass = getenv("DB_PASS"); // DB 패스워드
+        $dbChar = getenv("DB_CHAR"); // 문자 인코딩
 
         $this->db = new PDO("mysql:host={$dbHost};dbname={$dbName};charset={$dbChar}", $dbUser, $dbPass);
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
